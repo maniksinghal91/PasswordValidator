2592517a99b2418e6cff020cae71ad2fc64a2e5c
Manik Singhal <msinghal@sumologic.com>
2021-06-26 19:32:56 +0530

    Setup

---
+++ src/main/java/com/validator/PasswordValidator/PasswordValidatorApplication.java
@@ -0,0 +1,19 @@
+package com.validator.PasswordValidator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Configuration;
+
+@SpringBootApplication
+public class PasswordValidatorApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(PasswordValidatorApplication.class);
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(PasswordValidatorApplication.class, args);
+	}
+}
